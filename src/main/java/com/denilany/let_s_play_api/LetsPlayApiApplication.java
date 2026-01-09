a418508a895308e18c34249068f401ccc4ab1414
denilany <denilany1@gmail.com>
2026-01-09 17:50:17 +0300

    Initial commit: Project setup with MongoDB config and User model

---
+++ src/main/java/com/denilany/let_s_play_api/LetsPlayApiApplication.java
@@ -0,0 +1,13 @@
+package com.denilany.let_s_play_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsPlayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsPlayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denilany/let_s_play_api/LetsPlayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denilany.let_s_play_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsPlayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
